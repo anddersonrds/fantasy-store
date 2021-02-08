4d44846fd56b0764b6089f6b222e6be19c1dbeb4
Anderson Rodrigues <anddersonrds@gmail.com>
2021-02-08 06:03:37 -0300

    Add Home page

---
+++ src/pages/Home/Home.spec.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import { Home } from './Home'
+import { ProductCardProps } from '../../shared/components/ProductCard'
+import { Category } from '../../shared/types'
+
+jest.mock("../../shared/components/ProductCard", () => ({
+  ProductCard: ({ datum }: ProductCardProps) => {
+    const { name, price, image } = datum
+    return (
+      <div>
+        {name} {price} {image}
+      </div>
+    )
+  }
+}))
+
+describe("Home", () => {
+  describe('while loading', () => {
+    it('renders loader', () => {
+      const mockUseProducts = () => ({
+        categories: [],
+        isLoading: true,
+        error: false
+      })
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />)
+
+      expect(container.innerHTML).toMatch('Loading...')
+    })
+  })
+
+  describe('with error', () => {
+    it('renders error message', () => {
+      const mockUseProducts = () => ({
+        categories: [],
+        isLoading: false,
+        error: true
+      })
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />)
+
+      expect(container.innerHTML).toMatch('Error')
+    })
+  })
+
+  describe('with data', () => {
+    const category: Category = {
+      name: "Category Foo",
+      items: [{
+        name: "Product foo",
+        price: 55,
+        image: "/test.jpg"
+      }]
+    }
+
+    it('renders categories with products', () => {
+      const mockUseProducts = () => ({
+        categories: [category],
+        isLoading: false,
+        error: false
+      })
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />)
+
+      expect(container.innerHTML).toMatch("Category Foo")
+      expect(container.innerHTML).toMatch('Product foo 55 /test.jpg')
+    })
+  })
+})
