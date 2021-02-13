570dc1d26281e44a4ed63b7d6406f3a684ec107a
Anderson Rodrigues <anddersonrds@gmail.com>
2021-02-13 17:51:18 -0300

    Add Checkout Page

--- src/pages/Checkout/Checkout.spec.tsx
+++ src/pages/Checkout/Checkout.spec.tsx
@@ -1,5 +1,48 @@
 import React from 'react'
+import { render } from '@testing-library/react'
 
-describe('Checkout', () => {
-  it.todo("renders correctly")
+import { Checkout } from './Checkout'
+import { CheckoutForm } from '../../shared/components/CheckoutForm'
+import { CheckoutList } from '../../shared/components/CheckoutList'
+
+jest.mock('../../shared/components/CheckoutForm', () => ({
+  CheckoutForm: jest.fn(() => null)
+}))
+
+jest.mock('../../shared/components/CheckoutList', () => ({
+  CheckoutList: jest.fn(() => null)
+}))
+
+describe("Checkout", () => {
+  afterEach(jest.clearAllMocks)
+
+  const products = [
+    {
+      name: "Product foo",
+      price: 0,
+      image: "image.png"
+    }
+  ]
+  const stubCartHook = () => ({
+    products,
+    removeFromCart: () => {},
+    totalPrice: () => 55
+  })
+
+  it("shows total price", () => {
+    const { container } = render(
+      <Checkout useCartHook={stubCartHook as any} />
+    )
+    expect(container.innerHTML).toMatch("Total: 55 Zm")
+  })
+
+  it("passes products to CheckoutList", () => {
+    render(<Checkout useCartHook={stubCartHook as any} />)
+    expect(CheckoutList).toHaveBeenCalledWith({ products }, {})
+  })
+
+  it("renders checkout form", () => {
+    render(<Checkout useCartHook={stubCartHook as any} />)
+    expect(CheckoutForm).toHaveBeenCalled()
+  })
 })
--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -3,20 +3,34 @@ import React from "react"
 import { useCart } from '../../shared/hooks/CartContext'
 import { CheckoutList } from '../../shared/components/CheckoutList'
 import { CheckoutForm } from '../../shared/components/CheckoutForm'
+import { postCheckout } from '../../shared/utils/api'
 
-export const Checkout = () => {
-  const { products, totalPrice } = useCart()
+interface CheckoutProps {
+  useCartHook?: typeof useCart
+}
+
+export const Checkout = ({ useCartHook = useCart }: CheckoutProps) => {
+  const { products, totalPrice, clearCart } = useCartHook()
+
+  const submitCheckout = async () => {
+    const { orderId } = await postCheckout({products})
+
+    clearCart()
+    window.location.assign(`/order/?orderId=${orderId}`)
+  }
 
   return (
     <section className="nes-container with-title">
       <h1 className="title">Checkout</h1>
+
       <div className="nes-container is-rounded checkout-list-wrapper">
         <p>You are going to buy:</p>
         <CheckoutList products={products} />
         <p>Total: {totalPrice()} Zm</p>
       </div>
+
       <p>Enter your payment credentials:</p>
-      <CheckoutForm />
+      <CheckoutForm submit={submitCheckout} />
     </section>
   )
 }
